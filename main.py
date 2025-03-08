269fba909a5d433959da080d7366d7abd26d680a
DysonLittleDev <dysonlittledev@gmail.com>
2025-03-08 15:33:10 -0500

    Added Phrase and PhrasePair classes. Rewriting some Score definitions to be able to analyze higher counterpoint species.

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ if __name__ == "__main__":
     mini = MiniMidi.MiniMidi()
     file = mini.openFile("MidiFiles/Test.mid")
 
-    score = MiniTuneLib.Score(file.tracks)
+    time_signature = mini.getTimeSignature(file.tracks[0])
+    score = MiniTuneLib.Score(file.tracks, file.ticks_per_beat, time_signature)
     #mini.playTracks(file.tracks, range(len(file.tracks)))
